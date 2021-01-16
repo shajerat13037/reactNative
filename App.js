a1cd07543839ea50b13b8ef830bf3b4f62c73db2
alishajerat <shajerat13037@gmail.com>
2021-01-16 11:51:33 +0330

    git Test 2

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ import AddPerson from "./components/AddPerson";
 
 const App = () => {
     const [persons, setPersons] = useState([
-        {name: "علی ali شجراتی", key: "1", completed: false},
+        {name: "علی  شجراتی", key: "1", completed: false},
         {name: "یوسف شجراتی", key: "2", completed: false}
     ]);
     const [person, setPerson] = useState("")
