4a047e97714db2f7ce3575def96c85b323a1b43b
alishajerat <shajerat13037@gmail.com>
2021-01-16 16:13:25 +0330

    Debuging With DevTolse Episod : 18

--- App.js
+++ App.js
@@ -1,29 +1,84 @@
-import React from 'react'
-import { StyleSheet, Text, View ,Image} from 'react-native'
+import React from 'react';
+import {View, Text, StyleSheet, Keyboard, Alert} from "react-native";
+import Header from "./components/Header"
+import {useState} from "react";
+import {FlatList} from "react-native";
+import Person from "./components/Person";
+import AddPerson from "./components/AddPerson";
+
 
 const App = () => {
+    console.log("Appication Executed");
+
+    let hello;
+    hello.toString();
+
+    const [persons, setPersons] = useState([
+        {name: "علی شجراتی", key: "1", completed: false},
+        {name: "یوسف شجراتی", key: "2", completed: false}
+    ]);
+    const [person, setPerson] = useState("")
+
+    const deleteHandler = (key) => {
+        setPersons((prevPersons) => prevPersons.filter((p) => p.key != key))
+    };
+
+    const submitHandler = () => {
+        if (person.length > 3) {
+
+            setPersons([...persons,
+                {
+                    name: person,
+                    key: Math.floor(Math.random() * 1000).toString(),
+                    completed: false
+                }
+            ]);
+            setPerson("");
+            Keyboard.dismiss();
+        } else {
+            Alert.alert("توجه", "باید بیش از سه کراکتر باشد", [
+                {text: "فهمیدم", onPress: () => console.log("")}
+            ]);
+        }
+    };
+
+    const completedHandler = (key) => {
+        const allPersons = [...persons];
+        const personIndex = allPersons.findIndex(p => p.key === key);
+        const person = allPersons[personIndex];
+        person.completed = !person.completed;
+        allPersons[personIndex] = person;
+        setPersons(allPersons);
+    }
+
     return (
         <View style={styles.container}>
-         <Text>hello World</Text>
-         {/* <Image source={require("./assets/icon.png")}/> */}
-        <Image source={{uri:"https://picsum.photos/200/300",
-        width:200,
-        height:300,
-        }}
-        blurRadius={0}
-        fadeDuration={1000}
-        />
+            {/*Header*/}
+            <Header/>
+            <View style={styles.body}>
+                {/*AddPerson*/}
+                <AddPerson setPerson={setPerson} person={person} submitHandler={submitHandler}/>
+                <FlatList
+                    data={persons}
+                    renderItem={({item}) => (<Person person={item}
+                                                     deleteHandler={deleteHandler}
+                                                     completedHandler={completedHandler}
+                    />)}
+                />
+            </View>
         </View>
-    )
-}
+    );
+};
 
-export default App
+export default App;
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-       alignItems:"center",
-       justifyContent:"center"
+        backgroundColor: "white"
     },
-  
-});
+    body: {
+        padding: 20,
+        flex: 1
+    }
+})
