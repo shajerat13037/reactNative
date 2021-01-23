59f53d2398e2ea15c20cc1e9fb33b7cdede13ca5
alishajerat <shajerat13037@gmail.com>
2021-01-23 12:41:58 +0330

    HeaderOptions Styles  Episod : 26

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import {NavigationContainer} from "@react-navigation/native"
-import {createStackNavigator} from '@react-navigation/stack'
+import { NavigationContainer } from "@react-navigation/native"
+import { createStackNavigator, HeaderStyleInterpolators } from '@react-navigation/stack'
 import About from './screens/About'
 import Home from './screens/Home'
 import Details from './screens/Details';
-const Stack= createStackNavigator();
+const Stack = createStackNavigator();
 const App = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator>
-<Stack.Screen name="Home" component={Home}/>
-<Stack.Screen name="About" component={About}/>
-<Stack.Screen name="Details" component={Details}
-initialParams={{id:"2",text:"خدانگهدار"}}
-/>
+            <Stack.Navigator screenOptions={{
+                headerStyle: {
+                    backgroundColor: "orange",
+
+                },
+                headerTitleStyle: {
+                    color: "white"
+                }
+            }}>
+                <Stack.Screen name="Home" component={Home} options={{
+                    title: "خانه",
+                    headerTitleAlign: "center",
+
+                }} />
+                <Stack.Screen name="About" component={About} />
+                <Stack.Screen name="Details" component={Details}
+                    initialParams={{ id: "2", text: "خدانگهدار" }}
+                    options={{ title: "جزئیات" }}
+                />
             </Stack.Navigator>
 
         </NavigationContainer>
@@ -24,11 +37,10 @@ initialParams={{id:"2",text:"خدانگهدار"}}
 export default App
 
 const styles = StyleSheet.create({
-    container:{
-       flex: 1,
-       backgroundColor: "white",
-       alignItems:"center",
-       justifyContent:"center"
+    container: {
+        flex: 1,
+        backgroundColor: "white",
+        alignItems: "center",
+        justifyContent: "center"
     }
- })
- 
+})
