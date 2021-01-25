8df08353db46005ffbbefa9d0eeabc5dcec6b72a
alishajerat <shajerat13037@gmail.com>
2021-01-25 18:10:04 +0330

    Live sicle Navigation Episod : 31

--- App.js
+++ App.js
@@ -1,50 +1,65 @@
-import React from 'react'
-import {Button,View,Text} from "react-native";
-import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
-import {NavigationContainer} from "@react-navigation/native";
-import {Ionicons} from "@expo/vector-icons";
-import {createStackNavigator} from "@react-navigation/stack"
-
-const Tab =createBottomTabNavigator();
-const Stack= createStackNavigator();
+import React,{useEffect,useCallback} from 'react'
+import { Button, View, Text } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer,useFocusEffect } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
+import { createStackNavigator } from "@react-navigation/stack"
 
 
+const Tab = createBottomTabNavigator();
+const SettingStack = createStackNavigator();
+const HomeStack = createStackNavigator();
 
-const HomeScreen = ({navigation}) => {
- return(
-     <Tab.Navigator>
-         <Tab.Screen name="Settings" component={Setting}/>
-         <Tab.Screen name="Profile" component={Profile}/>
-     </Tab.Navigator>
 
- )
-};
-const Setting = ({navigation}) => {
+
+
+const HomeScreen = ({ navigation }) => {
     return (
-     <View style={{flex:1 ,alignItems:"center",justifyContent:"center"}}>
-    <Text>Setting</Text>
-<Button title="informaton" onPress={()=>navigation.navigate("Information")}/>
-     </View>
+<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+<Text>Home+</Text>
+<Button title="informaton" onPress={() => navigation.navigate("Information")} />
+</View>
+    )
+};
+const Setting = ({ navigation }) => {
+    return (
+        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+            <Text>Setting</Text>
+            <Button title="Profile" onPress={() => navigation.navigate("Profile")} />
+        </View>
     );
 };
-const Profile = ({navigation}) => {
+const Profile = ({ navigation }) => {
+    // useEffect(()=>{
+    //     return navigation.addListener("focus",()=>{
+    //         alert("hello");
+    //     });
+    // },[navigation])
+
+useFocusEffect(
+    useCallback(()=>{
+        //وقتی صفه متمرکز شد
+        alert('Focused');
+        return ()=>{
+            //وقتی ار متمرکز خارچ شد
+            alert("UnFocused")
+        }
+    })
+)
+
     return (
-     <View style={{flex:1 ,alignItems:"center",justifyContent:"center"}}>
-<Text>Profile</Text>
-    
-     </View>
+        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+            <Text>Profile</Text>
+
+        </View>
     );
 };
 
-const Information = ({navigation}) => {
+const Information = ({ navigation }) => {
     return (
-     <View style={{flex:1 ,alignItems:"center",justifyContent:"center"}}>
-         <Button onPress={()=>navigation.navigate("Home")} title="صفه خانه"/>
-     <View style={{marginBottom:20}}>
-
-     </View>
-     
-     </View>
+        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+            <Text>Information</Text>
+        </View>
     );
 };
 
@@ -54,12 +69,32 @@ const Information = ({navigation}) => {
 const App = () => {
     return (
         <NavigationContainer>
-       <Stack.Navigator>
-           <Stack.Screen name="Home" component={HomeScreen}/>
-           <Stack.Screen name="Information" component={Information}/>
-       </Stack.Navigator>
+            <Tab.Navigator>
+                <Tab.Screen name="first">
+                    {() => (
+                        <SettingStack.Navigator>
+                            <SettingStack.Screen name="Setting"
+                                component={Setting} />
+                            <SettingStack.Screen name="Profile"
+                                component={Profile} />
+                        </SettingStack.Navigator>
+                    )}
+                </Tab.Screen>
+                <Tab.Screen name="second">
+                    {() => (
+                        <HomeStack.Navigator>
+                            <HomeStack.Screen name="Home"
+                                component={HomeScreen} />
+                                <HomeStack.Screen name ="Information"
+                                component={Information}/>
+                        </HomeStack.Navigator>
+
+
+                    )}
+                </Tab.Screen>
+            </Tab.Navigator>
         </NavigationContainer>
-        );
+    );
 };
 
 export default App
