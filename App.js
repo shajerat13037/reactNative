a796e1557d8f12a92844ceba618675c3b73583fe
alishajerat <shajerat13037@gmail.com>
2021-01-25 14:35:34 +0330

    BottomTabNavigation Episod : 29

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import {Button,View} from "react-native";
-import {createDrawerNavigator,useIsDrawerOpen} from "@react-navigation/drawer";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import {NavigationContainer} from "@react-navigation/native";
-
+import {Ionicons} from "@expo/vector-icons"
 
 
 const HomeScreen = ({navigation}) => {
-    const isDrawerOpen=useIsDrawerOpen();
-    console.log(isDrawerOpen);
+   
+
     return (
      <View style={{flex:1 ,alignItems:"center",justifyContent:"center"}}>
          <Button onPress={()=>navigation.navigate("Information")} title="صفه اطلاعات"/>
-     <View style={{marginBottom:20}}></View>
+     <View style={{marginBottom:20}}>
+         <Ionicons name="ios-information-circle"/>
+     </View>
      </View>
     );
 };
@@ -21,43 +23,45 @@ const Information = ({navigation}) => {
      <View style={{flex:1 ,alignItems:"center",justifyContent:"center"}}>
          <Button onPress={()=>navigation.navigate("Home")} title="صفه خانه"/>
      <View style={{marginBottom:20}}>
-         <Button onPress={()=>navigation.openDrawer()} title="دراور"/>
 
      </View>
-     <View style={{marginBottom:20}}>
-         <Button onPress={()=>navigation.toggleDrawer()} title="toggle"/>
-
-     </View>
+     
      </View>
     );
 };
 
 
 
-const Drawer =createDrawerNavigator();
+const Tab =createBottomTabNavigator();
 
 const App = () => {
     return (
         <NavigationContainer>
-            <Drawer.Navigator initialRouteName="Home"  
-            drawerPosition="right"
-            drawerStyle={{
-                backgroundColor: "orange",
-                width:250,
-            }}
-            drawer
-            >
-                <Drawer.Screen 
-               
-                name="Home" component={HomeScreen}/>
-                <Drawer.Screen
-                 name="Information"
-                  component={Information}
-                  options={{
-                      title:"info",
-                  }}
-                  />
-            </Drawer.Navigator>
+          <Tab.Navigator screenOptions={({route})=>({
+              tabBarIcon:({focused,color,size})=>{
+                  let iconName; 
+                  if(route.name ==="Home"){
+                      iconName=focused ? "ios-information-circle":
+                      "ios-information-circle-outline"
+                  }else if(route.name=== "Information"){
+                      iconName = focused ? "ios-list-box" :"ios-list"
+                  }
+                  return(
+                    <Ionicons name={iconName} size={size} color={color}/>
+                  );
+              }
+          })}
+          tabBarOptions={{
+              activeTintColor:"tomato",
+              inactiveTintColor:"gray"
+          }}>
+              <Tab.Screen name="Home" component={HomeScreen}/>
+              <Tab.Screen name="Information" component={Information}
+              options={{
+             title:"info",
+             tabBarVisible:false,
+             }}/>
+          </Tab.Navigator>
         </NavigationContainer>
         );
 };
