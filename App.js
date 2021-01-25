2bf0f5b9850d674daff2d1bf0e91f6dcada4dcba
alishajerat <shajerat13037@gmail.com>
2021-01-25 21:21:36 +0330

    Rsponsive Platform ,Statusbar  Episod : 33

--- App.js
+++ App.js
@@ -1,108 +1,34 @@
-import React,{useEffect,useCallback} from 'react'
-import { Button, View, Text } from "react-native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { NavigationContainer,useFocusEffect } from "@react-navigation/native";
-import { Ionicons } from "@expo/vector-icons";
-import { createStackNavigator } from "@react-navigation/stack";
-import ModalScreen from './screens/ModalScreen';
-
-
-const Tab = createBottomTabNavigator();
-const SettingStack = createStackNavigator();
-const HomeStack = createStackNavigator();
-
-
-
-
-const HomeScreen = ({ navigation }) => {
-    return (
-<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-<Text>Home+</Text>
-<Button title="informaton" onPress={() => navigation.navigate("Information")} />
-</View>
-    )
-};
-const Setting = ({ navigation }) => {
-    return (
-        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-            <Text>Setting</Text>
-            <Button title="Profile" onPress={() => navigation.navigate("Profile")} />
-            <Button title="Open Modal" onPress={() => navigation.navigate("Modal")} />
-        </View>
-    );
-};
-const Profile = ({ navigation }) => {
-    // useEffect(()=>{
-    //     return navigation.addListener("focus",()=>{
-    //         alert("hello");
-    //     });
-    // },[navigation])
-
-useFocusEffect(
-    useCallback(()=>{
-        //وقتی صفه متمرکز شد
-        alert('Focused');
-        return ()=>{
-            //وقتی ار متمرکز خارچ شد
-            alert("UnFocused")
-        }
-    })
-)
-
-    return (
-        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-            <Text>Profile</Text>
-
-        </View>
-    );
-};
-
-const Information = ({ navigation }) => {
-    return (
-        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-            <Text>Information</Text>
-        </View>
-    );
-};
-
-
-
+import React from 'react'
+import { StyleSheet, Text, View ,StatusBar,Platform} from 'react-native'
 
 const App = () => {
     return (
-        <NavigationContainer>
-            <Tab.Navigator>
-                <Tab.Screen name="first">
-                    {() => (
-                        <SettingStack.Navigator>
-                            <SettingStack.Screen name="Setting"
-                                component={Setting} />
-                            <SettingStack.Screen name="Profile"
-                                component={Profile} />
-                        </SettingStack.Navigator>
-                    )}
-                </Tab.Screen>
-                <Tab.Screen name="second">
-                    {() => (
-                        <HomeStack.Navigator>
-                            <HomeStack.Screen name="Home"
-                                component={HomeScreen} />
-                                <HomeStack.Screen name ="Information"
-                                component={Information}/>
-                                <HomeStack.Screen name ="Modal"
-                                component={ModalScreen}
-                                options={{
-                                    headerShown:false
-                                }}/>
-                        </HomeStack.Navigator>
-
-
-                    )}
-                </Tab.Screen>
-            </Tab.Navigator>
-        </NavigationContainer>
-    );
-};
+        <View>
+            <Text style={styles.toplearn}>alishajerat</Text>
+        </View>
+    )
+}
 
 export default App
 
+
+const styles = StyleSheet.create({
+    toplearn:{
+        marginTop:StatusBar.currentHeight,
+        fontSize:     Platform.OS==="android" ? 18:20,
+        ...Platform.select({
+            ios:{
+                //require("./component.js")
+                fontSize:20,
+                fontFamily:"Roboto",
+                color:"tomato"
+            },
+            android:{
+                //require("./component.js")
+                fontSize:16,
+                color:"blue"
+            }
+        })
+  
+    }
+})
